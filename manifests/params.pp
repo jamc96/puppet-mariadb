43ea64b9f599db5ede70172ffb65b487cd948800
Jose Mejia <jamc.mejia@gmail.com>
2017-09-27 10:10:49 -0600

    Fixing issue with os_name declaration

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,13 +9,14 @@ class mariadb::params {
   $gpgcheck    = '1'
   $gpgkey      = 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB'
   $version_max = 10.3
+  $os_name     = downcase($operatingsystem)
   if $version <  $version_max{
     case $operatingsystemmajrelease {
       '7': {
-        $baseurl = "http://yum.mariadb.org/${version}/downcase(${operatingsystem})/7/x86_64/"
+        $baseurl = "http://yum.mariadb.org/${version}/${os_name}/7/x86_64/"
       }
       '6': {
-        $baseurl = "http://yum.mariadb.org/${version}/downcase(${operatingsystem})/6/x86_64/"
+        $baseurl = "http://yum.mariadb.org/${version}/${os_name}/6/x86_64/"
       }
       default: {
         fail('OS Release version not supported on this module')
