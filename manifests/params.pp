59a52c753fcd3d5a1952c50b6071653e9a7ae2b7
Jose Mejia <jamc.mejia@gmail.com>
2017-09-27 11:52:33 -0600

    Fixing issue with release comparation

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,3 @@
-# == Class: mariadb::para
-#
 class mariadb::params {
   $version      = 10
   $release      = 0.29
@@ -9,25 +7,25 @@ class mariadb::params {
   $enabled      = '1'
   $gpgcheck     = '1'
   $gpgkey       = 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB'
-  $version_max  = 10
-  $release_max  = 3.1
   $os_name      = downcase(facts['operatingsystem'])
   $full_version = "${version}${release}"
 
-  if $release_max >= $release {
-    case facts['operatingsystemmajrelease'] {
-      '7': {
-        $baseurl = "http://yum.mariadb.org/${full_version}/${os_name}/7/x86_64/"
-      }
-      '6': {
-        $baseurl = "http://yum.mariadb.org/${full_version}/${os_name}/6/x86_64/"
-      }
-      default: {
-        fail('OS Release version not supported on this module')
+  case $release {
+    '/[1-3]/': {
+      case facts['operatingsystemmajrelease'] {
+        '7': {
+          $baseurl = "http://yum.mariadb.org/${full_version}/${os_name}/7/x86_64/"
+        }
+        '6': {
+          $baseurl = "http://yum.mariadb.org/${full_version}/${os_name}/6/x86_64/"
+        }
+        default: {
+          fail('OS Release version not supported on this module')
+        }
       }
     }
-  }else {
-    fail('MariaDB version is not supported')
+    default: {
+      fail('MariaDB version is not supported')
+    }
   }
-
 }
