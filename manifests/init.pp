c707ff01ec8104b8dc861ffc9c9da65d44eabeaf
Jose Mejia <jamc.mejia@gmail.com>
2018-02-01 09:02:09 -0600

    chaning validation for versioning

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,51 +43,32 @@
 # Copyright 2017 Your name here, unless otherwise noted.
 #
 class mariadb(
-  Optional[Integer] $version                = undef,
-  Optional[Float] $release                  = undef,
+  Pattern[/latest|^[.+_0-9:~-]+$/] $version = undef,
   Pattern[/present|absent/] $package_ensure = 'present',
   Pattern[/present|absent/] $config_ensure  = 'present'
   ) inherits mariadb::params {
   # GLobal variables
   $use_version = $version ? {
+    'latest' => $::mariadb::parameter::latest_version,
     undef => $::mariadb::params::version,
     default => $version,
   }
-  $use_release = $release ? {
-    undef => $::mariadb::params::release,
-    default => $release,
-  }
   $use_package_ensure = $package_ensure ? {
-    undef => $::mariadb::params::package_ensure,
+    'present' => $::mariadb::params::package_ensure,
     default => $package_ensure,
   }
-  $use_config_ensure = $config_ensure ? {
-    undef => $::mariadb::params::config_ensure,
-    default => $config_ensure,
-  }
-  $use_repo_ensure = $use_package_ensure ? {
-    'absent' => $use_package_ensure,
+  $use_repo_ensure = $package_ensure ? {
+    'absent' => $package_ensure,
     default => $::mariadb::params::repo_ensure,
   }
-  $public_repo  = downcase($::mariadb::params::public_repo)
-  $full_version = "${use_version}.${use_release}"
+  $use_config_ensure = $config_ensure ? {
+    'present' => $::mariadb::params::config_ensure,
+    default => $config_ensure,
+  }
+  $public_repo  = $::mariadb::params::public_repo
   $os_name  = $mariadb::params::os_name
   # Validating release version
-  if $use_release < $::mariadb::params::release_max {
-      case $::operatingsystemmajrelease {
-        '7': {
-          $baseurl = "${public_repo}/${full_version}/${os_name}/${::operatingsystemmajrelease}/${::architecture}/"
-        }
-        '6': {
-          $baseurl = "${public_repo}/${full_version}/${os_name}/${::operatingsystemmajrelease}/${::architecture}/"
-        }
-        default: {
-          fail('OS Release version not supported on this module')
-        }
-      }
-    } else{
-      fail('MariaDB version is not supported')
-    }
+  $baseurl = "${public_repo}/${use_version}/${os_name}/${::operatingsystemmajrelease}/${::architecture}/"
 
   class { '::mariadb::install': } -> class { '::mariadb::config': } -> Class['::mariadb']
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,8 @@
 # == Class: mariadb::params
 #
 class mariadb::params {
-  $version          = 10
-  $release          = 0.29
+  $version          = '10.0.29'
+  $latest_version   = '10.3.4'
   $repo_ensure      = 'present'
   $repo_name        = 'mariadb'
   $descr            = 'MariaDB'
@@ -10,9 +10,6 @@ class mariadb::params {
   $gpgcheck         = '1'
   $public_repo      = 'https://yum.mariadb.org'
   $gpgkey           = "${public_repo}/RPM-GPG-KEY-MariaDB"
-  $version_max      = 10
-  $release_max      = 3.1
-  $full_version     = "${version}.${release}"
   $package_name     = 'MariaDB'
   $package_ensure   = 'present'
   $package_provider = 'yum'
